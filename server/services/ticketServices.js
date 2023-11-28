b99111b48d28a175e73a7ea4b1b6ca6adfe02e8a
Gabriel Milchev <gabromilchev9@gmail.com>
2023-11-28 11:55:27 +0200

    ✨ feat(Input Validation for Tickets):

--- server/services/ticketServices.js
+++ server/services/ticketServices.js
@@ -6,13 +6,36 @@ exports.create = (body) => {
     if (body.name === '') {
         throw new Error('Name is required')
     }
+    let nameRegex = /[a-zA-z ]+/g;
+    let matchName = body.name.match(nameRegex);
+
+
+    if (matchName == null) {
+        throw new Error('Please enter a valid name');
+    }
+
+    let phoneRegex = /[0-9]+/g;
+    let matchPhone = body.phoneNumber.match(phoneRegex);
+    if (matchPhone == null) {
+        throw new Error('Please enter a valid phone number');
+    }
 
     if (body.phoneNumber === '') {
         throw new Error('Phone number is required')
     }
+
+
+    let messageRegex = /[a-zA-z1-9. ,]+/;
+    let matchMessage = (body.message.match(messageRegex));
+    if (matchMessage.input !== matchMessage[0]) {
+        throw new Error('Message may only consist of letters and numbers');
+    }
+
+
     if (body.email === '') {
         throw new Error('Email is required')
     }
+
 
     if (body.message === '') {
         throw new Error('Message is required');
